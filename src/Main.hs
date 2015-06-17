9f52c1fd085e62bc4b219d38d670e7d4a7808ff9
Paul Meng <mno2@mno2.org>
2015-06-17 11:58:06 +0800

    feat(Main): Parse the arity and index of fucntion and arguments

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Main where
 
-import Control.Monad.IO.Class (liftIO)
-import System.Console.Haskeline
-import Text.Parsec (sepBy1, many1, (<|>), parse)
-import Text.Parsec.Char (string, char)
+import           Control.Monad.IO.Class (liftIO)
+import qualified Data.Text                              as T
+import           System.Console.Haskeline
+import           Text.Parsec (sepBy1, many1, (<|>), parse, ParsecT, Stream)
+import           Text.Parsec.Char (string, char)
+
 
 data Inst = Abs Int [Inst]
           | App Int Int
@@ -16,13 +21,36 @@ eval :: Prog -> IO ()
 eval = undefined
 
 
-absParser = many1 (char '解')
+absParser :: Stream s m Char => ParsecT s u m Inst
+absParser = do { arity_seq <- aritySeqParser
+               ; let arity = length arity_seq
+               ; return $ Abs arity []
+               }
+  where
+    aritySeqParser :: Stream s m Char => ParsecT s u m [Char]
+    aritySeqParser = many1 (char '解')
 
-appParser = many1 (char '接')
 
+appParser :: Stream s m Char => ParsecT s u m Inst
+appParser = do { idx_of_function_seq <- idxOfFunctionParser
+               ; return $ App 3 3
+               }
+  where 
+    idxOfFunctionParser :: Stream s m Char => ParsecT s u m [Char]
+    idxOfFunctionParser = many1 (char '接')
+
+    idxOfArgumentParser :: Stream s m Char => ParsecT s u m [Char]
+    idxOfArgumentParser = many1 (char '解')
+
+
+instParser :: Stream s m Char => ParsecT s u m Inst
 instParser = appParser <|> absParser
 
-programParser = instParser `sepBy1` (string "叫我")
+
+programParser :: Stream s m Char => ParsecT s u m Prog
+programParser = do { insts <- instParser `sepBy1` (string "叫我")
+                   ; return $ Prog insts
+                   }
 
 
 main :: IO ()
