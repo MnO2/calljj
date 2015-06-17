6d998d118be10e7ac5d4197145c35e2138b5bccf
Paul Meng <mno2@mno2.org>
2015-06-17 18:03:34 +0800

    feat(Main.hs): Put Parsec's symbol under P qualified name

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,9 @@ module Main where
 import           Control.Monad.IO.Class (liftIO)
 import qualified Data.Text                              as T
 import           System.Console.Haskeline
-import           Text.Parsec (sepBy1, many1, (<|>), parse, ParsecT, Stream)
-import           Text.Parsec.Char (string, char)
+import qualified Text.Parsec                            as P
+import           Text.Parsec ((<|>), ParsecT, Stream)
+import qualified Text.Parsec.Char                       as P
 
 
 data Inst = Abs Int [Inst]
@@ -24,23 +25,27 @@ eval = undefined
 absParser :: Stream s m Char => ParsecT s u m Inst
 absParser = do { arity_seq <- aritySeqParser
                ; let arity = length arity_seq
+               ; P.count arity appParser
                ; return $ Abs arity []
                }
   where
     aritySeqParser :: Stream s m Char => ParsecT s u m [Char]
-    aritySeqParser = many1 (char '解')
+    aritySeqParser = P.many1 (P.char '解')
 
 
 appParser :: Stream s m Char => ParsecT s u m Inst
 appParser = do { idx_of_function_seq <- idxOfFunctionParser
-               ; return $ App 3 3
+               ; idx_of_argument_seq <- idxOfArgumentParser
+               ; let idx_of_function = length idx_of_function_seq
+               ; let idx_of_argument = length idx_of_argument_seq
+               ; return $ App idx_of_function idx_of_argument
                }
   where 
     idxOfFunctionParser :: Stream s m Char => ParsecT s u m [Char]
-    idxOfFunctionParser = many1 (char '接')
+    idxOfFunctionParser = P.many1 (P.char '接')
 
     idxOfArgumentParser :: Stream s m Char => ParsecT s u m [Char]
-    idxOfArgumentParser = many1 (char '解')
+    idxOfArgumentParser = P.many1 (P.char '解')
 
 
 instParser :: Stream s m Char => ParsecT s u m Inst
@@ -48,7 +53,7 @@ instParser = appParser <|> absParser
 
 
 programParser :: Stream s m Char => ParsecT s u m Prog
-programParser = do { insts <- instParser `sepBy1` (string "叫我")
+programParser = do { insts <- instParser `P.sepBy1` (P.string "叫我")
                    ; return $ Prog insts
                    }
 
@@ -64,7 +69,7 @@ main = runInputT defaultSettings loop
         Just "quit" -> return ()
         Just input -> do 
           outputStrLn $ "Input was: " ++ input
-          case (parse programParser "" input) of
+          case (P.parse programParser "" input) of
             Left err -> liftIO $ print err
             Right xs -> liftIO $ putStrLn "Hello Word"
           loop
